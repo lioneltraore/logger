6abd63df788648cd5669d2e3274f5f3c2b34fc24
Traore Leonel <ltraore@m2i-sa.com>
2024-07-18 17:19:28 +0000

    add logger method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/api/pti/callback')
+  create(@Body() data: any) {
+    return this.appService.logCallback(data);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async logCallback(data: any): Promise<string> {
+    console.log('data: ' + JSON.stringify(data));
+    return 'IT WORKS';
   }
 }
